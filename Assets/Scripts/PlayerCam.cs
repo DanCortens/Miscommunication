d348d85b1f0ca2de5eb11e6b0dadb099b353a1f7
Dan Cortens <dan_cortens@hotmail.com>
2022-12-19 14:08:17 -0500

    player cont sstart

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    [SerializeField] private float sensX; //horizontal mouse sensitivity
+    [SerializeField] private float sensY; //vertical    ^^     ^^ 
+
+    [SerializeField] private Transform orientation;
+    [SerializeField] private Transform playerPos;
+
+    private float xRot;
+    private float yRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensX;
+
+        yRot += mouseX;
+        xRot = Mathf.Clamp(xRot - mouseY, -90f,90f);
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0f);
+        orientation.rotation = Quaternion.Euler(0f, yRot, 0f);
+        transform.position = playerPos.position + new Vector3(0f, 0.25f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform orientation;
+
+    private float horzInput;
+    private float vertInput;
+
+    private Vector3 moveDir;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
