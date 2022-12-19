d9f54cc6db251f3d05d50fb77ebfba0bc0f324d4
Dan Cortens <dan_cortens@hotmail.com>
2022-12-19 15:07:44 -0500

    player cont rough draft done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float groundedDrag;
+    [SerializeField] private bool grounded;
+    [SerializeField] private float accel;
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private bool canJump;
+    [SerializeField] private float airSlowdown;
+    [Header("Misc")]
     [SerializeField] private Transform orientation;
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask groundLayer;
+    
 
     private float horzInput;
     private float vertInput;
@@ -24,6 +36,44 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
+        GetInput();
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    private void GetInput()
+    {
+        horzInput = Input.GetAxisRaw("Horizontal");
+        vertInput = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.Space) && canJump && grounded)
+        {
+            canJump = false;
+            Jump();
+            Invoke("CanJump", jumpCooldown);
+        }
+    }
+    private void Movement()
+    {
+        rb.drag = (grounded) ? groundedDrag : 0f;
+        moveDir = orientation.forward * vertInput + orientation.right * horzInput;
+        float multiplier = (grounded) ? accel : accel * airSlowdown;
+        rb.AddForce(moveDir * moveSpeed * multiplier, ForceMode.Force);
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (flatVel.magnitude > moveSpeed)
+        {
+            flatVel = (flatVel.normalized) * moveSpeed;
+            rb.velocity = new Vector3(flatVel.x, rb.velocity.y, flatVel.z);
+        }
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+    private void CanJump()
+    {
+        canJump = true;
     }
 }
