dfd41356652704bab50bcb135050dbb70c1343de
qiuand <101195886+qiuand@users.noreply.github.com>
2022-12-19 17:34:25 -0500

    Interact script
    Add empty interact script

---
+++ Assets/Scripts/InteractScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractScript : MonoBehaviour
+{
+    public LayerMask interactableMask=8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, interactableMask))
+        {
+            if (Input.GetKeyDown("f"))
+            {
+
+            }
+        }
+    }
+}
